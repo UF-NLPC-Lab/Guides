abe19624193ed83690a6e9274803d8342422fd3d
Ethan L. Mines <elmines@users.noreply.github.com>
2025-01-08 16:16:08 -0500

    WIP: Populate postgres

--- conceptnet/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-HOST_GROUP=$(groups | awk '{print $1}')
-APPTAINERENV_TMPDIR=/tmp apptainer build -F --build-arg HOST_USER=$(whoami) A.sif conceptnet.def
-
-apptainer build -F --sandbox sandbox/ A.sif
--- conceptnet/data_commands.sh
+++ conceptnet/data_commands.sh
@@ -5,3 +5,8 @@ service postgresql start
 /usr/lib/postgresql/12/bin/createuser -U postgres -d $HOST_USER
 psql -U postgres -c "create database $HOST_USER"
 psql -c "create database conceptnet5"
+
+cd /conceptnet5
+mkdir data
+python3 -m pip install -e '.[vectors]'
+./build.sh
