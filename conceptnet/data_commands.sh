ecde53ba86e2b33c293c9c39eca793ca04a125fb
Ethan L. Mines <elmines@users.noreply.github.com>
2025-01-09 07:52:56 -0500

    Build the conceptnet python lib

--- conceptnet/data_commands.sh
+++ conceptnet/data_commands.sh
@@ -7,6 +7,12 @@ psql -U postgres -c "create database $HOST_USER"
 psql -c "create database conceptnet5"
 
 cd /conceptnet5
-mkdir data
+# TODO: try and move this to the .def file
+python3 -m venv ./local_env
+source ./local_env/bin/activate
+# venv doesn't have wheel installed by default, and it's needed to install some other packages. Not sure why python's topological sort can't pick up on that
+python3 -m pip install wheel
 python3 -m pip install -e '.[vectors]'
-./build.sh
+
+mkdir data
+#./build.sh
