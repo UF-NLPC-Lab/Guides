6ae5b9f2cc5d8b5ef29440e2762806e3d383e741
Ethan L. Mines <elmines@users.noreply.github.com>
2025-01-09 13:34:44 -0500

    Automatically create postgres db

--- conceptnet/data_commands.sh
+++ conceptnet/data_commands.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 HOST_USER=$(whoami)
-service postgresql start
-/usr/lib/postgresql/12/bin/createuser -U postgres -d $HOST_USER
-psql -U postgres -c "create database $HOST_USER"
+PG_BIN=/usr/lib/postgresql/12/bin
+cd /conceptnet5
+
+rm -rf pg_data && mkdir pg_data
+$PG_BIN/initdb pg_data/
+$PG_BIN/pg_ctl -D pg_data/ -l pg.log start
+psql -d postgres -c "create database $HOST_USER"
 psql -c "create database conceptnet5"
 
-cd /conceptnet5
-# TODO: try and move this to the .def file
 python3 -m venv ./local_env
 source ./local_env/bin/activate
 # venv doesn't have wheel installed by default, and it's needed to install some other packages. Not sure why python's topological sort can't pick up on that
 python3 -m pip install wheel
 python3 -m pip install -e '.[vectors]'
 
-mkdir data
-#./build.sh
+./build.sh
+
+$PG_BIN/pg_ctl stop -D pg_data/
